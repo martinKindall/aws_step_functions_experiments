183647af32090b13968dd83a9da9c2a4dbc20e1e
Martin <mart256@gmail.com>
2022-09-29 12:08:29 +0200

    Started the definition of the step function

--- lib/cdk_step_function_orders-stack.ts
+++ lib/cdk_step_function_orders-stack.ts
@@ -1,19 +1,44 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
+import * as sfn_tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 
 export class CdkStepFunctionOrdersStack extends Stack {
+  private createOrderLambda: lambda.Function;
+  private cancelOrderLambda: lambda.Function;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkStepFunctionOrdersQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    this.sagaStepFunction();
+  }
+
+  private sagaStepFunction() {
+    const orderFailed = new sfn.Fail(this, "No se pudo crear la orden");
+    const orderSucceeded = new sfn.Succeed(this, "Se ha creado la orden");
+
+    const cancelOrder = new sfn_tasks.LambdaInvoke(this, 'CancelOrder', {
+      lambdaFunction: this.cancelOrderLambda,
+      resultPath: '$.CancelOrderResult'
+    })
+        .addRetry({maxAttempts: 3})
+        .next(orderFailed);
+
+    const createOrder = new sfn_tasks.LambdaInvoke(this, 'CreateOrder', {
+      lambdaFunction: this.createOrderLambda,
+      resultPath: '$.CreateOrderResult'
+    }).addCatch(cancelOrder, {
+      resultPath: '$.CreateOrderError'
     });
 
-    const topic = new sns.Topic(this, 'CdkStepFunctionOrdersTopic');
+    const definition = sfn.Chain
+        .start(createOrder)
+        .next(orderSucceeded);
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const saga = new sfn.StateMachine(this, 'OrderSaga', {
+      definition,
+      timeout: Duration.minutes(5)
+    });
   }
 }
